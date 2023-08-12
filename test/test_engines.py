4bb2d201cae802c4c7f79f3e2a734663e1fc8cac
BrumaMan <bmazurowski07@gmail.com>
2023-08-12 22:16:22 +0100

    Renamed file to test_engines.py

---
+++ test/test_engines.py
@@ -0,0 +1,35 @@
+import unittest
+from datetime import datetime
+
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+
+
+class TestCapuletEngine(unittest.TestCase):
+    def test_capulet_engine_needs_service_true(self):
+        current_mileage = 30001
+        last_service_mileage = 0
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
+
+    def test_capulet_engine_needs_service_false(self):
+        current_mileage = 30000
+        last_service_mileage = 0
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
+
+
+class TestSternmanEngine(unittest.TestCase):
+    def test_sternman_engine_needs_service_true(self):
+        warning_light_is_on = True
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertTrue(engine.needs_service())
+
+    def test_sternman_engine_needs_service_false(self):
+        warning_light_is_on = False
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertFalse(engine.needs_service())
+        
+        
+if __name__ == '__main__':
+    unittest.main()
